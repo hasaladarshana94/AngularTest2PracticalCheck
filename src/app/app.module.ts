15726c1777414fc883bdeec667d3ccea06bd1701
hasaladarshana94 <hasaladarshana2@gmail.com>
2022-08-24 18:48:04 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ToastrModule } from 'ngx-toastr';
+
+import { AppComponent } from './app.component';
+import { StockModule } from './stock/stock.module';
+import { ErrorMessageComponent } from './error-message/error-message.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ErrorMessageComponent
+  ],
+  imports: [
+    BrowserModule,
+    StockModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
